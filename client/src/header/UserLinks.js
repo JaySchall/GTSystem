5d3203e756c6c654874dea84a16c687cf19d60e8
Mudcup <104184919+Mudcup@users.noreply.github.com>
2023-11-26 17:00:46 -0500

    header implementation

---
+++ client/src/header/UserLinks.js
@@ -0,0 +1,15 @@
+import './UserLinks.css';
+
+export default function Navbar(){
+    return(
+        <div>
+            <span class="contact-links contact-links-2">
+                <ul>
+                    <li>
+                        <a href="/user/login">Sign In</a>
+                    </li>
+                </ul>
+            </span>
+        </div>
+    )
+}
